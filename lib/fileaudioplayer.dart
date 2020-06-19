3cff0c3b435d6ddc005b9b0fb700e4423b48710a
smocken78 <sm@mocken.org>
2020-06-19 17:19:47 +0200

    Update fileaudioplayer.dart

--- lib/fileaudioplayer.dart
+++ lib/fileaudioplayer.dart
@@ -1,11 +1,47 @@
 import 'dart:async';
-
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/services.dart';
 
 class FileAudioPlayer {
   static const MethodChannel _channel = const MethodChannel('fileaudioplayer');
 
   FileAudioPlayer();
+
+  Map<String, File> _assetCache = Map<String, File>();
+
+  loadAssets(List<String> fileNames) {
+    fileNames.forEach((element) async {
+      File f = await _fetchToMemory(element);
+      _assetCache[element] = f;
+    });
+  }
+
+  cleanUp() {
+    _assetCache.keys.forEach((element) async {
+      _assetCache[element].delete();
+    });
+    _assetCache.clear();
+  }  
+
+  Future<File> _fetchToMemory(String fileName) async {
+    final file = File('${(await getTemporaryDirectory()).path}/$fileName');
+    await file.create(recursive: true);
+    return await file
+        .writeAsBytes((await _fetchAsset(fileName)).buffer.asUint8List());
+  }
+
+  Future<ByteData> _fetchAsset(String fileName) async {
+    return await rootBundle.load(fileName);
+  }
+
+  Future<void> playAsset(String asset) async {
+    try {
+      await start(_assetCache[asset].path);
+    } on PlatformException catch (e) {
+      print("Stream start error : $e");
+    }
+  }
 
   Future<void> start(String path) async {
     try {
