9af1bca90422de66bb2fb931d4d0882566adcfcb
smocken78 <sm@mocken.org>
2020-06-20 14:43:23 +0200

    Update SwiftFileaudioplayerPlugin.swift

--- ios/Classes/SwiftFileaudioplayerPlugin.swift
+++ ios/Classes/SwiftFileaudioplayerPlugin.swift
@@ -40,7 +40,7 @@ public class SwiftFileaudioplayerPlugin: NSObject, FlutterPlugin, AVAudioPlayerD
         
         if (url != nil){
             do {
-                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, options: AVAudioSessionCategoryOptions.interruptSpokenAudioAndMixWithOthers)
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, options: AVAudioSession.CategoryOptions.interruptSpokenAudioAndMixWithOthers)
                 try AVAudioSession.sharedInstance().setActive(true)
 
                 try audioPlayer = AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.wav.rawValue, setVolume:1.0)
@@ -63,7 +63,7 @@ public class SwiftFileaudioplayerPlugin: NSObject, FlutterPlugin, AVAudioPlayerD
         audioPlayer?.stop()
         
         do {
-            try AVAudioSession.sharedInstance().setActive(false, with: AVAudioSessionSetActiveOptions.notifyOthersOnDeactivation)
+            try AVAudioSession.sharedInstance().setActive(false, with: AVAudioSession.CategoryOptions.notifyOthersOnDeactivation)
             self.flutterResult!(true)
         } catch {
             print("AVAudioSession stop error: \(error)")
@@ -100,7 +100,7 @@ public class SwiftFileaudioplayerPlugin: NSObject, FlutterPlugin, AVAudioPlayerD
     
     public func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
         do {
-            try AVAudioSession.sharedInstance().setActive(false,with: AVAudioSessionSetActiveOptions.notifyOthersOnDeactivation)
+            try AVAudioSession.sharedInstance().setActive(false,with: AVAudioSession.CategoryOptions.notifyOthersOnDeactivation)
             self.flutterResult!(true)
         } catch {
             print("AVAudioSession audioPlayerDidFinishPlaying error: \(error)")
